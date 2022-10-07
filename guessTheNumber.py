2a141638e2ca07aba5871b59c528ba62fbcd1001
Megan Carnegie <115034668+megancarnegie@users.noreply.github.com>
2022-10-06 18:06:40 -0600

    Add files via upload

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -10,7 +10,28 @@ def guess(x):
         elif guess > random_number:
             print("Sorry, guess again. Too high.")
 
-    print(f"Yay, congrats. You guessed the number {random_number}")
+    print(f"Yay, congrats. You guessed the number {random_number}!!")
         
 
-guess(10)
+#guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ' '
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else :
+            guess = low # could also be high b/c low = high
+        guess = random.randint(low, high)
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)? ").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f"Yay! The computer guessed your number, {guess}, correctly!!")
+
+
+computer_guess(10)
